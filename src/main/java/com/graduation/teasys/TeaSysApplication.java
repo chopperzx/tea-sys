02bfd1ea0a25ea84c4318aa367f6326124aa8a3c
chopper <leecherry984@gmail.com>
2024-12-25 16:26:42 +0800

    v0.0

---
+++ src/main/java/com/graduation/teasys/TeaSysApplication.java
@@ -0,0 +1,13 @@
+package com.graduation.teasys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeaSysApplication.class, args);
+    }
+
+}
